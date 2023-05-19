6a3b52471ffe3a30cad233dcc2472aeb0e3229bf
Abhishekgowda <abhishek@emproto.com>
2023-05-19 10:34:33 +0530

    added liquibase

--- src/main/java/io/abhishek/todoapp/TodoAppApplication.java
+++ src/main/java/io/abhishek/todoapp/TodoAppApplication.java
@@ -1,13 +1,29 @@
 package io.abhishek.todoapp;
 
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class TodoAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TodoAppApplication.class, args);
+    }
 
+    @Bean
+    public SpringLiquibase primaryDatabase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+
+        liquibase.setTag("primary");
+        liquibase.setBeanName("primary");
+        liquibase.setChangeLog("classpath:db/changelog/db.changelog-master.xml");
+        liquibase.setDataSource(dataSource);
+
+        return liquibase;
+    }
 }
